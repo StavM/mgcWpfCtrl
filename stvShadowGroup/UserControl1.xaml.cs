1f238ef66d6bceecde82a919d21faab3445fc0f3
StavM <stavmann2@gmail.com>
2017-08-05 17:47:40 +0300

    initial upload

---
+++ stvShadowGroup/UserControl1.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace stvShadowGroup
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+
+    //[Designer("System.Windows.Forms.Design.ControlDesigner, System.Design")]
+    //[DesignerSerializer("System.ComponentModel.Design.Serialization.TypeCodeDomSerializer , System.Design", "System.ComponentModel.Design.Serialization.CodeDomSerializer, System.Design")]
+    public class GroupHoster_WinformsHost : System.Windows.Forms.Integration.ElementHost
+    {
+        protected UserControl1 _usrctrl = new UserControl1();
+
+        public GroupHoster_WinformsHost()
+        {
+            base.Child = _usrctrl;
+        }
+    }
+}
